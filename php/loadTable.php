b9f7b5350aae1b924b83d3d961019225889b8c52
cjb654 <sirhcnt@gmail.com>
2022-06-20 11:58:12 +0100

    table

--- php/loadTable.php
+++ php/loadTable.php
@@ -10,23 +10,46 @@
     die("Connection failed: " . $conn->connect_error);
     }
     
-    $sql = "SELECT player, score, races, mean_score, mean_position FROM scores";
+    $sql = "SELECT player, score, position FROM races";
     $result = $conn->query($sql);
 
     echo '<div class="shown" id="scoreboard">';
+    $sortedPlayers = [];
     
     if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $player = [$row["player"], $row["score"], $row["position"], 1];
+            if(empty($sortedPlayers)){
+                array_push($sortedPlayers, $player);
+            }
+            else{
+                foreach($sortedPlayers as $sortedPlayer){
+                    if ($sortedPlayer[0] == $player[0]){
+                        $total_score = ($sortedPlayer[1] * $sortedPlayer[3]);
+                        $total_position = ($sortedPlayer[2] * $sortedPlayer[3]);
+                        $player[3] += $sortedPlayer[3];
+                        $player[2] += $total_position;
+                        $player[2] = $player[2] / $player[3];
+                        $player[1] += $total_score;
+                        $player[1] = $player[1] / $player[3];
+                        $sortedPlayer = $player;
+                        break 3;
+                    }
+                }
+                array_push($sortedPlayers, $player);
+            }
+        }
+
         echo '<table id="scoreTable"><tr>';    
         echo '<th>'.'Player'.'</th>';
-        echo '<th>'.'Total Score'.'</th>';
         echo '<th>'.'Races Played'.'</th>';
         echo '<th>'.'Avearge Score'.'</th>';
         echo '<th>'.'Average Position'.'</th>';
         echo '</tr>';
 
-        while($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . $row["player"]. "</td><td>" . $row["score"] . "</td><td>"
-            . $row["races"]. "</td><td>" . $row["mean_score"] . "</td><td>" . $row["mean_position"] . "</td></tr>";
+        foreach($sortedPlayers as $player){
+            echo "<tr><td>" . $player[0]. "</td><td>"
+            . $player[3]. "</td><td>" . $player[1] . "</td><td>" . $player[2] . "</td></tr>";
         }
         echo '</table>';
         echo '</div>';
